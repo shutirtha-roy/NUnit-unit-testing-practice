06e6a6ee393f17f7ad7ce973637a5a91a44896d7
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-12-26 10:58:06 +0600

    FizzBuzzTests Implemented

--- src/UnitSolution/Unit.NonDependency.Tests/FizzBuzzTests.cs
+++ src/UnitSolution/Unit.NonDependency.Tests/FizzBuzzTests.cs
@@ -16,7 +16,45 @@ namespace Unit.NonDependency.Tests
         [TestCase(45)]
         public void GetOutput_WhenDivisibleBy3And5_GetFizzBuzz(int number)
         {
-            string expected = "FizzBuzz";
+            var expected = "FizzBuzz";
+
+            var result = FizzBuzz.GetOutput(number);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        [TestCase(3)]
+        [TestCase(9)]
+        [TestCase(12)]
+        public void GetOutput_WhenDivisibleBy3_GetFizz(int number)
+        {
+            var expected = "Fizz";
+
+            var result = FizzBuzz.GetOutput(number);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        [TestCase(5)]
+        [TestCase(10)]
+        public void GetOutput_WhenDivisibleBy5_GetBuzz(int number)
+        {
+            var expected = "Buzz";
+
+            var result = FizzBuzz.GetOutput(number);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        [TestCase(11)]
+        [TestCase(17)]
+        [TestCase(12)]
+        public void GetOutput_WhenNotDivisibleBy3Or5_GetParameterNumberInString(int number)
+        {
+            var expected = number.ToString();
 
             var result = FizzBuzz.GetOutput(number);
 
