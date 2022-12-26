4607c819e6648b0bb4fda76aec7db8ea6cef14a4
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-12-26 13:04:45 +0600

    DemeritPointsCalculatorTests Implemented

--- src/UnitSolution/Unit.NonDependency.Tests/DemeritPointsCalculatorTests.cs
+++ src/UnitSolution/Unit.NonDependency.Tests/DemeritPointsCalculatorTests.cs
@@ -1,14 +1,53 @@
 ﻿using NUnit.Framework;
+using Shouldly;
+using System;
+using UnitLibrary.ClassesWithNoDependencies;
 
 namespace Unit.NonDependency.Tests
 {
     public class DemeritPointsCalculatorTests
     {
+        private DemeritPointsCalculator _demirPointsCalculator;
+
         [SetUp]
         public void Setup()
         {
+            _demirPointsCalculator = new DemeritPointsCalculator();
         }
 
+        [Test]
+        [TestCase(302)]
+        [TestCase(-2)]
+        public void CalculateDemeritPoints_WhenSpeedLessThanZeroOrGreaterThanMaxSpeed_ThrowArgumentOutOfRangeException(int speed)
+        {
+            Should.Throw<ArgumentOutOfRangeException>(() =>
+                _demirPointsCalculator.CalculateDemeritPoints(speed)
+            );
+        }
+
+        [Test]
+        [TestCase(65)]
+        [TestCase(32)]
+        [TestCase(0)]
+        [TestCase(66)]
+        public void CalculateDemeritPoints_WhenSpeedIsLessThanEqualToSpeedLimit_ReturnZero(int speed)
+        {
+            var expected = 0;
+
+            var result = _demirPointsCalculator.CalculateDemeritPoints(speed);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        [TestCase(165, 20)]
+        [TestCase(165, 10)]
+        public void CalculateDemitPoints_WhenSpeedIsGreaterThanSpeedLimitAndLessThanMaxSpeed_ReturnPoints(int speed, int expected)
+        {
+            var result = _demirPointsCalculator.CalculateDemeritPoints(speed);
+
+            Assert.AreEqual(expected, result);
+        }
 
     }
 }
